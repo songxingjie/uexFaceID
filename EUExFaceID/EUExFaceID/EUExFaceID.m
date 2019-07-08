6f9d61dc404e83ccf94a7bf30cebbe7b70c0d2c3
songxingjie <1771344212@qq.com>
2019-07-08 15:56:48 +0800

    修改面部不识别问题

--- EUExFaceID/EUExFaceID/EUExFaceID.m
+++ EUExFaceID/EUExFaceID/EUExFaceID.m
@@ -9,9 +9,9 @@
 #import "EUExFaceID.h"
 #import <LocalAuthentication/LocalAuthentication.h>
 
-
 static const NSInteger kUexFaceIDNoError = 0;
-static const NSInteger kUexFaceIDNotAvailable = -6; // -6 = LAErrorTouchIDNotAvailable
+
+
 
 @implementation EUExFaceID
 
@@ -19,83 +19,48 @@ static const NSInteger kUexFaceIDNotAvailable = -6; // -6 = LAErrorTouchIDNotAva
 - (NSNumber *)canAuthenticateForFaceID:(NSMutableArray *)inArguments{
     ACArgsUnpack(NSDictionary *info) = inArguments;
     NSNumber *mode = numberArg(info[@"mode"]);
-    if (!NSClassFromString(@"LAContext")) {
-        //8.0以下的系统
-        return @(kUexFaceIDNotAvailable);
-    }
-    
+   
     LAContext *ctx = [[LAContext alloc] init];
     NSError *error = nil;
-    LAPolicy policy = LAPolicyDeviceOwnerAuthenticationWithBiometrics;
+    LAPolicy policy = LAPolicyDeviceOwnerAuthentication;
     
     if (@available(iOS 11.0, *) && mode.integerValue == 1) {
         if (ctx.biometryType == LABiometryTypeFaceID){
            policy = LAPolicyDeviceOwnerAuthentication;
         }
     }
-    
+
     if(![ctx canEvaluatePolicy:policy error:&error]){
         ACLogDebug(@"FaceID is unavailable: %@",error.localizedDescription);
         NSLog(@"canAuthenticateForFaceID canEvaluatePolicy =========== error.code = %ld",error.code);
         return @(error.code);
     }
-
     return @(kUexFaceIDNoError);
 }
 
-/*
- error.code  :
-      1.LAErrorAuthenticationFailed   身份验证失败
- 
-      2.LAErrorUserCancel   用户在认证时点击取消
- 
-      3.LAErrorUserFallback 用户点击输入密码取消指纹验证
- 
-      4.LAErrorSystemCancel 身份认证被系统取消(按下Home键或电源键)
- 
-      5.LAErrorTouchIDNotEnrolled  用户未录入指纹
- 
-      6.LAErrorPasscodeNotSet 设备未设置密码
- 
-      7.LAErrorTouchIDNotAvailable  该设备为设置FaceID
- 
-      8.LAErrorTouchIDLockout   连续五次密码错误,FaceID被锁定.
- 
-      9.LAErrorAppCancel 用户不能控制情况下App被挂起
- 
- */
 
 //开始面部验证(验证FaceID是否通过evaluatePolicy)
 - (void)authenticateWithFaceID:(NSMutableArray *)inArguments{
     ACArgsUnpack(NSDictionary *info,ACJSFunctionRef *cb) = inArguments;
-    
+
     NSString *hint = stringArg(info[@"hint"]);
     NSNumber *mode = numberArg(info[@"mode"]);
-    
-    
+
+
     void (^callback)(NSInteger resultCode) = ^(NSInteger resultCode){
         [cb executeWithArguments:ACArgsPack(@(resultCode))];
     };
-    
-    if (!NSClassFromString(@"LAContext")){
-        callback(kUexFaceIDNotAvailable);
-        return;
-    }
-    
+
+  
     LAContext* ctx = [[LAContext alloc] init];
     NSError* error = nil;
-    LAPolicy policy = LAPolicyDeviceOwnerAuthenticationWithBiometrics;
+    LAPolicy policy = LAPolicyDeviceOwnerAuthentication;
     if (@available(iOS 11.0, *) && mode.integerValue == 1) {
         if (ctx.biometryType == LABiometryTypeFaceID){
              policy = LAPolicyDeviceOwnerAuthentication;
         }
     }
-    if (![ctx canEvaluatePolicy:policy error:&error]) {
-        //不支持面部识别
-        callback(error.code);
-         NSLog(@"authenticateWithFaceID canEvaluatePolicy =========== error.code = %ld",error.code);
-        return;
-    }
+   //注意iOS 11.3之后需要配置Info.plist权限才可以通过Face ID验证哦!不然只能输密码啦...
     [ctx evaluatePolicy:policy localizedReason:hint reply:^(BOOL success, NSError * _Nullable error) {
         if (success) {
             //验证成功
@@ -116,8 +81,9 @@ static const NSInteger kUexFaceIDNotAvailable = -6; // -6 = LAErrorTouchIDNotAva
     };
     
     NSError* error = nil;
-    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
-        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:hint reply:^(BOOL success, NSError *error) {
+    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:&error]) {
+        //注意iOS 11.3之后需要配置Info.plist权限才可以通过Face ID验证哦!不然只能输密码啦...
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthentication localizedReason:hint reply:^(BOOL success, NSError *error) {
             if (success) {
                 //验证成功
                 callback(YES,nil);
@@ -131,7 +97,6 @@ static const NSInteger kUexFaceIDNotAvailable = -6; // -6 = LAErrorTouchIDNotAva
         callback(NO,error.localizedDescription);
     }
 }
-
 
 
 @end
